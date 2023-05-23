d43791d72e44a68421bfe4981f47660771eebb44
Nariman <Nariman079i>
2023-05-23 16:02:45 +0300

    edd filters

--- offermvp/barkan/api/urls.py
+++ offermvp/barkan/api/urls.py
@@ -7,8 +7,10 @@ from .views import *
 router = routers.SimpleRouter()
 router.register(r'customers',CustomerAPIView)
 router.register(r'history',HistoryAPIView)
-router.register(r'guide', GuideAPIView),
 router.register(r'services',ServiceAPIView)
 urlpatterns = [
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('api/v1/guide/', GuideListView.as_view()),
+    path('api/v1/guide/create/', GuideCreateView.as_view()),
+    path('api/v1/guide/<int:pk>/', GuideDetailView.as_view())
 ]
--- offermvp/barkan/api/views.py
+++ offermvp/barkan/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import *
-
+from django.db.models import Q
+from rest_framework.generics import ListAPIView,RetrieveAPIView,RetrieveUpdateDestroyAPIView, CreateAPIView
 class CustomerAPIView(ModelViewSet):
     serializer_class = CustomerSeializer
     queryset = Customer.objects.all()
@@ -15,7 +16,38 @@ class HistoryAPIView(ModelViewSet):
     serializer_class = HistorySeializer
     queryset = History.objects.all()
 
+    
+class GuideListView(ListAPIView):
+    serializer_class = GuideSeializer
 
-class GuideAPIView(ModelViewSet):
+    def get_queryset(self):
+        search = self.request.query_params.get('search')
+        sort = self.request.query_params.get('sort')
+        if search:
+            return Guide.objects.filter(
+                Q(surname__icontains=search) |
+                Q(name__icontains=search) |
+                Q(patronymic__icontains=search) |
+                Q(tel__icontains=search))
+        elif sort:
+            return Guide.objects.all().order_by(sort)
+        else:
+            return Guide.objects.all()
+    
+class GuideCreateView(CreateAPIView):
     serializer_class = GuideSeializer
     queryset = Guide.objects.all()
+    
+class GuideDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = GuideSeializer
+    queryset = Guide.objects.all()
+
+# class GuideAPIView(ModelViewSet):
+#     serializer_class = GuideSeializer
+
+#     def get_queryset(self):
+#         experience = self.request.query_params('experience') 
+#         salary = self.request.query_params('salary') 
+#         if experience == "all" and salary == 'all':
+#             return Guide.objects.all()
+#         return Guide.objects.filter(Q(experience=experience) | Q(salary=salary))
